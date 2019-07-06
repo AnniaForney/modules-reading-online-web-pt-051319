fc1781e75e7e13da9beebb0c8dc58ffadf92262c
Annia Forney <anniaforney@gmail.com>
2019-07-06 02:31:16 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,10 +1,11 @@
-class Kid
-  include Dancer
-  extend MetaDancing
+require_relative './dance_module.rb'
+require_relative './class_methods_module.rb'
 
+class Kid
+  include Dance
+  extend MetaDancing
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
-end
