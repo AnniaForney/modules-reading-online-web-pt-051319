08841807342851bfd5a3cb2400b56723a527c169
Annia Forney <anniaforney@gmail.com>
2019-07-06 02:32:32 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -14,6 +14,7 @@ module FancyDance
     def take_a_bow
       "Thank you, thank you. It was a pleasure to dance for you all."
     end
+  end
 
   module ClassMethods
 
@@ -21,3 +22,4 @@ module FancyDance
       "This class produces objects that love to dance."
     end
   end
+end 
